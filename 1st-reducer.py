6daf7b2fb14f00d73ef10fd520841b5a4a008b35
foresights <49715124+foresights@users.noreply.github.com>
2021-07-12 09:38:45 +0100

    Add files via upload

---
+++ 1st-reducer.py
@@ -0,0 +1,16 @@
+import sys
+def reduce_func(token,results):
+    return token,sum(results)                        
+last_k=None
+results=[]
+for tokens in sys.stdin:
+    k,v=tokens.strip().split('|')  
+    if k!=last_k and last_k is not None:
+        k_result,v_result=reduce_func(last_k,results)
+        print(k_result+"|"+str(v_result))
+        results=[]
+    last_k=k
+    results.append(int(v))
+if last_k is not None:
+    k_result,v_result=reduce_func(last_k,results)
+    print(k_result+"|"+str(v_result))     
