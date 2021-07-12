9f36af20454b13fc348ec164fdd056fbc7e7e4bd
foresights <49715124+foresights@users.noreply.github.com>
2021-07-12 09:40:01 +0100

    Delete 1st-mapper.py

--- 1st-mapper.py
+++
@@ -1,8 +0,0 @@
-import sys
-def map_func(sents):
-    data=sents.strip().split('\t')
-    sent=data[2]
-    for token in sent.strip().split():
-for tokens in sys.stdin:
-    for key,value in map_func(tokens):  
-        print(key+"|"+str(value))
